748e71e28e30269fd5e72cd65d952469f8438bce
Sergio Lindo <sergiolindo.empresa@gmail.com>
2018-06-02 21:40:59 +0200

    Add alias 'duh' for command du

---
+++ src/duh.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This file is part of 'bash-helper'.
+#
+# Copyright 2018 Sergio Lindo <sergiolindo.empresa@gmail.com>
+#
+# 'bash-helper' is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# 'bash-helper' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# You should have received a copy of the GNU General Public License along with
+# 'bash-helper'. If not, see <http://www.gnu.org/licenses/>.
+
+# Name: du.sh
+#
+# Synopsis:
+#   du.sh
+#   Example: du.sh
+#
+# Description:
+#   Alias for bash command du.
+#   -h "Human-readable" output. Use unit suffixes:
+#      Byte, Kilobyte, Megabyte, Gigabyte, Terabyte and Petabyte.
+#
+du -h
